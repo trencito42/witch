f8a70e3de41a907b76161f2591794831fadb0dd8
trencito42 <trencito42@users.noreply.github.com>
2026-09-19 10:56:34 +0200

    Fix top-level await in worker/combined.ts for PM2

--- worker/combined.ts
+++ worker/combined.ts
@@ -1,4 +1,11 @@
 import { runWorkerLoop } from "./index";
 import { runSchedulerLoop } from "./scheduler";
 
-await Promise.all([runWorkerLoop(), runSchedulerLoop()]);
+async function main() {
+  await Promise.all([runWorkerLoop(), runSchedulerLoop()]);
+}
+
+main().catch((err) => {
+  console.error("Worker/Scheduler fatal error:", err);
+  process.exit(1);
+});
